c75ecef48dd4a1347eccbf5ab84896fb627b19ba
richierich180108 <richierich180108@gmail.com>
2026-08-21 21:43:42 +0700

    kelar juga mff

--- project2/project2.py
+++ project2/project2.py
@@ -8,11 +8,31 @@ class karakter:
         print(f'[{self.nama}] - Level {self.lv} | gear: {self.gear}')
 
 
-char1 = karakter('doctor strange',70, 'CTP of Energy')
-char2 = karakter('storm',80, 'Mighty CTP of Rage')
-char3 = karakter('ghost rider', 80, 'CTP of Rage')
+class roster:
+    def __init__(self):
+        self.daftarchar = []
+
+    def tambahchar(self, karakterbaru):
+        self.daftarchar.append(karakterbaru)
+        print(f'Berhasil menambahkan {karakterbaru.nama} ke dalam roster!')
+
+    def tampilkansemua(self):
+        print('\n=== DAFTAR ROSTER MFF===')
+        print(f'total karakter adalah {len(self.daftarchar)}')
+        for kar in self.daftarchar:
+            kar.showstat(   )
+        print('================\n')
 
 
-char1.showstat()
-char2.showstat()
-char3.showstat()
+
+char1 = karakter("Doctor Strange", 80, "CTP of Rage")
+char2 = karakter("Spider-Man", 70, "CTP of Authority")
+char3 = karakter("Loki", 80, "CTP of Energy")
+r = roster()
+r.tambahchar(char1)
+r.tambahchar(char2)
+r.tambahchar(char3)
+
+r.tampilkansemua()
+
+
